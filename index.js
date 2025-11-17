fe2a9accefa23a846300cb6aaa3d81e318a69db9
Othallia <vianiputriocta6@gmail.com>
2025-11-17 11:42:35 +0700

    membuat koneksi database

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public")); // Ini folder untuk file HTML nanti
+
+// --- KONEKSI DATABASE ---
+// Settingan sesuai punya kamu
+const dbPool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "ookwlan24", 
+  database: "api_key",   
+  port: 3307,            
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
